3c820335aa3809ae1d30b8c64976e87b41608fee
athilarhim <athilaraheem@gmail.com>
2021-11-24 10:52:51 +0530

    created dashboard

--- healthigo/src/App.js
+++ healthigo/src/App.js
@@ -1,11 +1,31 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // import { Link } from "react-router-dom";
 
-import './App.css';
+import User from "./components/user-interface/user";
+import Home from "./components/user-interface/home";
+import Signup from "./components/user-interface/Signup";
+import Login from "./components/user-interface/Login";
+
+// import { Router } from "express";
+
+//import { AuthContext, FirebaseContext } from './store/FirebaseContext';
 
 function App() {
   return (
-    <div className="App">
-      <button className="mt-5">gghh</button>
-      <div className="mt-5"></div>
+    <div>
+      {/* <BrowserRouter> */}
+      <Router>
+        <Routes>
+          <Route path="/user" element={<User />}></Route>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+
+
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ healthigo/src/components/user-interface/home.js
@@ -0,0 +1,44 @@
+import React from "react";
+function home() {
+  return (
+  <div className="container">
+      <h1 className="mt-5">User Dashboard</h1>
+      <div className="row mt-5">
+        <div className="card col-md-4" style={{ width: "16epm" }}>
+      <h2>Consult Doctor</h2>
+      <a href="/user" className="btn btn-success">Go to page</a>
+    </div>
+
+    <div className="card col-md-4" style={{ width: "16epm" }}>
+      <h2>Lab test</h2>
+      <a href="/user" className="btn btn-success">Go to user page</a>
+    </div>
+
+
+    <div className="card col-md-4" style={{ width: "16epm" }}>
+      <h2>Order medicine</h2>
+      <a href="/user" className="btn btn-success">Go to user page</a>
+    </div>
+    </div>
+
+  <div className="row mt-5">
+    <div className="card col-md-4" style={{ width: "16epm" }}>
+      <h2>Blood bank</h2>
+      <a href="/user" className="btn btn-success">Go to user page</a>
+    </div>
+
+    <div className="card col-md-4" style={{ width: "16epm" }}>
+      <h2>Diet suggestion</h2>
+      <a href="/user" className="btn btn-success">Go to user page</a>
+    </div>
+
+    <div className="card col-md-4" style={{ width: "16epm" }}>
+      <h2>Health status</h2>
+      <a href="/user" className="btn btn-success">Go to user page</a>
+    </div>
+    </div>
+  </div>
+  );
+}
+
+export default home;
