02f92f7e4ead94eebd202acf766d2fa049d078e8
aarathib <47327151+aarathib@users.noreply.github.com>
2021-11-24 23:58:52 +0530

    Add files via upload

---
+++ healthigo/labtest.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function labtest() {
+    return (
+        <div>
+            <div id="intro" className="bimage shadow-2-strong">
+                <div className="mask" >
+                  <div className="container d-flex align-items-center justify-content-center text-center h-100">
+                    <div className="mt-5">
+                      <h1 className="mb-3 mt-5">Get Your Lab Tests Scheduled Online!</h1>
+                      <a className="btn btn-outline-dark btn-lg m-2" href="https://pharmeasy.in/diagnostics/all-tests" role="button"
+                        rel="nofollow" target="_blank">Take Your Test</a>
+                      <a className="btn btn-outline-dark btn-lg m-2" href="https://ddrcsrl.com/reports.php" target="_blank"
+                        role="button">Get Test Report</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+    )
+}
+
+export default labtest
