d1bcee1592dcd33301964ef139a8e069f2f88935
Anna-Pramod <50614862+Anna-Pramod@users.noreply.github.com>
2021-11-24 22:02:21 +0530

    Details of the blood group

---
+++ healthigo/Blood_groups.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function apositive() {
+    return (
+        <div>
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">S No</th>
+      <th scope="col">Amount Available</th>
+      <th scope="col">Location</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>10 units</td>
+      <td>Kerala</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>5 units</td>
+      <td>Karnatka</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>2 units</td>
+      <td>Goa</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    )
+}
